8c17a43cd896a69166d2fbb51fd4f52731f59c55
hakuunabatata <emachadomartins1999@gmail.com>
2021-10-17 12:20:24 -0300

    create header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export const Header = () => (
+  <header className={styles.headerContainer}>
+    <div className={styles.headerContent}>
+      <img src="/images/logo.svg" alt="ig.news" />
+      <nav>
+        <a className={styles.active}>Home</a>
+        <a>Posts</a>
+      </nav>
+    </div>
+  </header>
+);
