cff74282a7bb6023e2d7212ad8acd3682404b22f
hakuunabatata <emachadomartins1999@gmail.com>
2021-10-17 16:01:12 -0300

    create signinbutton component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export const Header = () => (
   <header className={styles.headerContainer}>
     <div className={styles.headerContent}>
-      <img src="/images/logo.svg" alt="ig.news" />
+      <Image src="/images/logo.svg" alt="ig.news" width={100} height={100} />
       <nav>
         <a className={styles.active}>Home</a>
         <a>Posts</a>
       </nav>
+
+      <SignInButton />
     </div>
   </header>
 );
