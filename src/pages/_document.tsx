4541e50b54af4a0aadbe9d207f97d41f6cdc9654
hakuunabatata <emachadomartins1999@gmail.com>
2021-10-17 11:54:57 -0300

    add global styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <title>ig.news</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import styled from "../styles/home.module.scss";
-
 export default function Home() {
   return (
-    <h1 className={styled.title}>
-      Hello
-      <span>World</span>
+    <h1>
+      Hello <span>World</span>
     </h1>
   );
 }
